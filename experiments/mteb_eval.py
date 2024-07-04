0df93534b821f84dad37f6bc2f6d279b1be87356
vaibhavad <vaibhavadlakha95@gmail.com>
2024-07-04 19:38:17 +0000

    documentation and format

--- experiments/mteb_eval.py
+++ experiments/mteb_eval.py
@@ -4,10 +4,18 @@ import json
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, default="McGill-NLP/LLM2Vec-Meta-Llama-3-8B-Instruct-mntp-supervised")
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="McGill-NLP/LLM2Vec-Meta-Llama-3-8B-Instruct-mntp-supervised",
+    )
     parser.add_argument("--task_name", type=str, default="STS16")
-    parser.add_argument("--task_to_instructions_fp", type=str, default="test_configs/mteb/task_to_instructions.json")
-    parser.add_argument("--output_folder", type=str, default="results")
+    parser.add_argument(
+        "--task_to_instructions_fp",
+        type=str,
+        default="test_configs/mteb/task_to_instructions.json",
+    )
+    parser.add_argument("--output_dir", type=str, default="results")
 
     args = parser.parse_args()
 
@@ -20,4 +28,4 @@ if __name__ == "__main__":
     model = mteb.get_model(args.model_name, **model_kwargs)
     tasks = mteb.get_tasks(tasks=[args.task_name])
     evaluation = mteb.MTEB(tasks=tasks)
-    results = evaluation.run(model, output_folder=args.output_folder)
+    results = evaluation.run(model, output_folder=args.output_dir)
